e7f4237f4ebb4ec79416e3c8da1cff9793c7811d
EdgarHg <iscehg@gmail.com>
2016-08-27 01:34:56 -0500

    commit integracion

---
+++ app.js
@@ -0,0 +1,33 @@
+angular.module('App', ['ngAnimate'])
+	.controller("ctrValidaciones",['$scope', function($scope){
+		$scope.objMensajes = {
+			nombre: false, 
+			apellidoPat: false
+		};
+
+		$scope.mdlUsuario = {
+			nombre: "", 
+			apellidoPat: ""
+		};
+
+		$scope.BtnGuardarUsuario = function(){
+			if($scope.mdlUsuario.nombre === ""){
+				$scope.objMensajes.nombre = true;
+			}else if($scope.mdlUsuario.apellidoPat === ""){
+				$scope.objMensajes.apellidoPat = true;
+			}else{
+				GuardarUsuario();
+			}
+		};
+
+		$scope.CerrarMensajes = function(estatus, input){
+			if(estatus){
+				$scope.objMensajes[input] = false;
+			}
+		};
+    
+		function GuardarUsuario(){
+			console.log("Enviando al Servicio para Guardar...");
+		}
+	}])
+
